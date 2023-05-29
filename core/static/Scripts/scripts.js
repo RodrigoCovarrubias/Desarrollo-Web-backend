cc22f485b95421be6784ecdcfaed7480605f9c25
RodrigoCovarrubias <114321460+RodrigoCovarrubias@users.noreply.github.com>
2023-05-28 23:17:33 -0400

    footer fix

--- core/static/Scripts/scripts.js
+++ core/static/Scripts/scripts.js
@@ -9,3 +9,34 @@
 
     return true;
   }
+
+  // Add this JavaScript code after including jQuery and your other scripts
+$(document).ready(function() {
+  // Handle form submission
+  $("#login-button").click(function(e) {
+    e.preventDefault();
+
+    var email = $("#exampleInputEmail1").val();
+    var password = $("#exampleInputPassword1").val();
+    $.ajax({
+      url: "/login",
+      method: "POST",
+      data: {
+        email: email,
+        password: password
+      },
+      success: function(response) {
+        console.log(response);
+       
+        window.location.href = "/dashboard";
+      },
+      error: function(xhr, status, error) {
+        console.log(error);
+
+        $("#error-message").text("Login failed. Please try again.");
+        $("#error-message").show();
+      }
+    });
+  });
+});
+
